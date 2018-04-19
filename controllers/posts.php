1683d7937b88d312896186644531ffd2fc6ff7f6
ilona <ilona.kolossova@khk.ee>
2018-04-19 21:10:35 +0300

    posts_view.php faili loomine

--- controllers/posts.php
+++ controllers/posts.php
@@ -16,7 +16,8 @@ class posts extends Controller
     }
 
     function view(){
-
+        $post_id = $this->params[0];
+        $this->post = get_first("SELECT * FROM posts NATURAL JOIN users WHERE post_id='$post_id'");
     }
 
     /**
